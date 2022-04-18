348127a7547a3c64c17eaca040bc49da14544bab
ZJL1128 <33411815+ZJL1128@users.noreply.github.com>
2022-04-18 14:37:45 +0800

    Add files via upload

---
+++ week1/find_matching.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def find_matching(L, pattern):
+    index = []
+    for n,i in enumerate(L):
+        if pattern in i:
+            index.append(n)
+
+
+
+    return index 
+
+def main():
+    pass
+    index = find_matching(["sensitive", "engine", "rubbish", "comment"], "en")
+    print(index)
+
+if __name__ == "__main__":
+    main()
