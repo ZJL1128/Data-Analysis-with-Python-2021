c074242db4b90d33bd13497a52170b895057815f
ZJL1128 <33411815+ZJL1128@users.noreply.github.com>
2022-04-18 14:28:54 +0800

    Add files via upload

---
+++ week1/compliment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from unicodedata import name
+
+
+def main():
+    # Enter you solution here
+    name=input("What country are you from? ")
+    print("I have heard that" , name ,"is a beautiful country.")
+
+if __name__ == "__main__":
+    main()
