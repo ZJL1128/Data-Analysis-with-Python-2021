77b379d775b39803843105062f31420f64368d3d
ZJL1128 <33411815+ZJL1128@users.noreply.github.com>
2022-04-18 14:36:25 +0800

    Add files via upload

---
+++ week1/distinct_characters.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def distinct_characters(L):
+    d = {}
+    for i in L:
+        l = len(set(i))
+        d[i] = l
+    return d
+
+def main():
+    print(distinct_characters(["check", "look", "try", "pop"]))
+
+if __name__ == "__main__":
+    main()
