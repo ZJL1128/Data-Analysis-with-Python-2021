3e1cfe32e04d7bcafca2c8d705c70fd4cd0ac7cb
ZJL1128 <33411815+ZJL1128@users.noreply.github.com>
2022-04-18 14:35:52 +0800

    Add files via upload

---
+++ week1/interleave.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def interleave(*lists):
+    l=[]
+    for i in zip(*lists):
+        l.extend(i)
+    
+    return l
+
+def main():
+    print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
+
+if __name__ == "__main__":
+    main()
