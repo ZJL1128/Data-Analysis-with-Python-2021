16103083b5b257801598cc78427252e8acccf0da
ZJL1128 <33411815+ZJL1128@users.noreply.github.com>
2022-04-24 15:15:46 +0800

    06

---
+++ week2/file_count.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+def file_count(filename):
+    lines = 0
+    words = 0
+    chars = 0
+    with open(filename,'r') as f:
+        
+        for i in f:
+            lines += 1
+            w = re.findall(r'(\S+)',i)
+            words += len(w)
+            chars += len(i)
+
+
+
+    return (lines, words, chars)
+
+def main():
+    for i in sys.argv[1:]:
+        l,w,c = file_count(i)
+        print(f'{l}\t{w}\t{c}\t{i}')
+
+
+if __name__ == "__main__":
+    main()
