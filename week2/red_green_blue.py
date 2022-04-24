3bb83e7e9c9f84c965fb6308b659f64a86ccd94b
ZJL1128 <33411815+ZJL1128@users.noreply.github.com>
2022-04-24 15:13:53 +0800

    Add files via upload

---
+++ week2/red_green_blue.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import re
+
+def red_green_blue(filename="src/rgb.txt"):
+    with open (filename,'r') as f:
+        res =[]
+
+        for n,i in enumerate(f):
+            if n != 0:
+                pattern = r'(\s*\d{1,3})\s+(\d{1,3})\s+(\d{1,3})\s+(.+)'
+                temp = re.match(pattern,i)
+                temp = temp.groups()
+                temp = f'{temp[0]}\t{temp[1]}\t{temp[2]}\t{temp[3]}'
+                res .append(temp)
+    return res
+
+
+def main():
+
+    print(red_green_blue())
+
+if __name__ == "__main__":
+    main()
