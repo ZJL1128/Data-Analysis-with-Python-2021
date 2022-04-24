aad7c28b3f7231f228c0aa7dc6c9e024816dcca8
ZJL1128 <33411815+ZJL1128@users.noreply.github.com>
2022-04-24 15:24:22 +0800

    12

---
+++ week2/row_and_column_vectors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def get_row_vectors(a):
+    b = np.split(a,np.size(a,0))
+    return b
+
+def get_column_vectors(a):
+    b = np.split(a,np.size(a,1),axis = 1)
+    
+    return b
+
+def main():
+    np.random.seed(0)
+    a=np.random.randint(0,10, (4,4))
+    print("a:", a)
+    print("Row vectors:", get_row_vectors(a))
+    print("Column vectors:", get_column_vectors(a))
+
+if __name__ == "__main__":
+    main()
