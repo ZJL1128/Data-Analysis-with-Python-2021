75e60688368ba58a0f39720ca0811506587277c3
ZJL1128 <33411815+ZJL1128@users.noreply.github.com>
2022-05-01 11:16:22 +0800

    1

---
+++ week3/column_comparison.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def column_comparison(a):
+    c =  a[:,1] > a[:,-2]
+
+    return np.array(a[c])
+    
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
